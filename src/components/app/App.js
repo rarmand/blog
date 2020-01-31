fc109ce584ee0ef349b8c4675973f723053e3a9e
rarmand <holik.alek@gmail.com>
2020-01-31 02:02:12 +0100

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>The inner space</p>
+      </header>
+      <main>
+        <p>Something to tell</p>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
