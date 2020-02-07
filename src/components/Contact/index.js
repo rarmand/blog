e76d9b54f155b5066446e748403ffecd6f14dc96
rarmand <holik.alek@gmail.com>
2020-02-07 19:33:48 +0100

    fix in styling of Contact box

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -13,8 +13,15 @@ function Contact(props) {
 
   const dataView = Object.keys(dataObj).map(key => (
     <div className="contactContainer__inputContainer">
-      <p className="contactContainer__inputContainer--p">{dataObj[key]}:</p>
-      <input className="contactContainer__inputContainer--input"></input>
+      <label for={key} className="contactContainer__inputContainer--label">
+        {dataObj[key]}:
+      </label>
+      <input
+        type="input"
+        id={key}
+        name={key}
+        className="contactContainer__inputContainer--input"
+      ></input>
     </div>
   ));
 
@@ -22,10 +29,15 @@ function Contact(props) {
     <div className="contactContainer">
       <p className="contactContainer__title">{title}</p>
       <div className="contactContainer__dataContainer">
-        {dataView}
-        <button className="contactContainer__dataContainer--button">
-          {buttonInfo}
-        </button>
+        <form className="contactContainer__dataContainer--form">
+          {dataView}
+          <button
+            type="submit"
+            className="contactContainer__dataContainer--button"
+          >
+            {buttonInfo}
+          </button>
+        </form>
       </div>
     </div>
   );
